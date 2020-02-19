28792f1c6ec6b7da2db45b9797cac8e80d8dd031
David Magno <jdtm12@outlook.com>
2020-02-18 19:50:51 -0800

    Assignment 3

---
+++ module05/files/install_script.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+sudo yum -y install git
+sudo yum -y install nodejs
+sudo yum -y install mongodb-server
+
+sudo systemctl enable mongod
+sudo systemctl start mongod
+
+
+sudo useradd todoapp -p "P@ssw0rd"
+sudo usermod -aG wheel todoapp
+#sudo su - todoapp
+
+sudo mkdir /home/todoapp/app
+# sudo chown todoapp /home/todoapp/app
+sudo cd /home/todoapp/app
+sudo git clone https://github.com/timoguic/ACIT4640-todo-app.git /home/todoapp/app
+sudo chmod -R 755 /home/todoapp/app
+sudo npm install --prefix /home/todoapp/app
+
+sudo cp /home/admin/database.js /home/todoapp/app/config/
+sudo chown -R todoapp /home/todoapp/app
+#Firewall
+sudo firewall-cmd --zone=public --add-port=8080/tcp
+sudo firewall-cmd --zone=public --list-all
+
+#nginx
+sudo yum -y install nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
+sudo cp /home/admin/nginx.conf /etc/nginx/
+sudo systemctl restart nginx
+
+sudo chmod 755 /home/todoapp
+
+#selinux
+sudo setenforce 0
+
+#daemon
+sudo cp /home/admin/todoapp.service /etc/systemd/system/todoapp.service
+sudo systemctl daemon-reload
+
+sudo systemctl enable todoapp
+sudo systemctl start todoapp
+
+sudo systemctl restart nginx
+sudo systemctl restart todoapp
